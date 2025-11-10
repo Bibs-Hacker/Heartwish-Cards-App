382f379a415a1587d02267f1c5415d662f054da9
Bibs-Hacker <199905918+Bibs-Hacker@users.noreply.github.com>
2025-11-10 16:12:34 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'heartwish-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://files.catbox.moe/vjjtdz.jpg',
+  'https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js',
+  'https://html2canvas.hertzen.com/dist/html2canvas.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
